0acabfd8d33d11327462b32b9b215bf29218a418
Lukas Hartmann <lukas.hartmann@vertigis.com>
2025-11-07 17:38:46 +0100

    Add Filter and Map utility functions for generic slice operations

---
+++ util/array_helpers/helpers.go
@@ -0,0 +1,19 @@
+package array_helpers
+
+func Filter[T any](source []T, condition func(item T) bool) []T {
+	filtered := []T{}
+	for _, elem := range source {
+		if allowed := condition(elem); allowed {
+			filtered = append(filtered, elem)
+		}
+	}
+	return filtered
+}
+
+func Map[T any, U any](source []T, mapFn func(item T) U) []U {
+	mapped := make([]U, 0, len(source))
+	for _, elem := range source {
+		mapped = append(mapped, mapFn(elem))
+	}
+	return mapped
+}
