ddbd983f378fce630aeb2517810ab0347af095cb
Lukas Hartmann <lukas.hartmann@vertigis.com>
2025-12-09 17:21:47 +0100

    Implement Day 07 logic in part01 function; update Day07 to process input and count splits

--- 2025/7/day.go
+++ 2025/7/day.go
@@ -1,6 +1,11 @@
 package aoc_2025_day07
 
-import aoc_helpers "github.com/Djosar/advent-of-code/util/aoc-helpers"
+import (
+	"regexp"
+
+	aoc_helpers "github.com/Djosar/advent-of-code/util/aoc-helpers"
+	"github.com/Djosar/advent-of-code/util/slice_helpers"
+)
 
 const testInput = `.......S.......
 ...............
@@ -27,8 +32,53 @@ func Day07() *aoc_helpers.AOCDay {
 }
 
 func part01(d *aoc_helpers.AOCDay) (any, error) {
+	lines := d.Lines
+	startRegex := regexp.MustCompile("S")
+	// splitPointRegex := regexp.MustCompile(`\^`)
 
-	return 0, nil
+	// x indexes
+	beamTracker := make([][]int, len(lines))
+	splitCount := 0
+
+	for y, ln := range lines {
+		if y == 0 {
+			match := startRegex.FindStringIndex(ln)
+			beamTracker[0] = []int{
+				match[0],
+			}
+			continue
+		}
+
+		beamStore := []int{}
+		for idx, char := range ln {
+			currentBeamTrack := beamTracker[y-1]
+			idxRelevant := slice_helpers.Count(currentBeamTrack, idx) >= 1
+
+			if !idxRelevant {
+				continue
+			}
+
+			strChar := string(char)
+			switch strChar {
+			case ".":
+				beamStore = append(beamStore, idx)
+
+			case "^":
+				left := idx - 1
+				right := idx + 1
+				if left >= 0 && slice_helpers.Count(beamStore, left) == 0 {
+					beamStore = append(beamStore, left)
+				}
+				if right < len(ln) && slice_helpers.Count(beamStore, right) == 0 {
+					beamStore = append(beamStore, right)
+				}
+				splitCount++
+			}
+		}
+		beamTracker[y] = beamStore
+	}
+
+	return splitCount, nil
 }
 
 func part02(d *aoc_helpers.AOCDay) (any, error) {
